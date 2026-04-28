1c118feedee41c21d8ee1a805887708dab43eee4
riddhitanwani7 <riddhitanwani7@gmail.com>
2026-04-28 14:55:34 +0530

    fix: add correct frontend URL to CORS

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ app.use(cors({
     'http://localhost:5173', 
     'http://localhost:3000',
     'https://foodstaaa.vercel.app',
+    'https://foodstaaaaa.vercel.app',
     process.env.FRONTEND_URL
   ].filter(Boolean),
   credentials: true,
